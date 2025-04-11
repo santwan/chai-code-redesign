756721145ff33f3f4064824f6382ef5a18b7670a
santwan <santwanpathak1@gmail.com>
2025-04-11 12:16:50 +0530

    installed  react-carousel and slider for Carousel effect on Mobile version of the chaicode of twitter section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './index.css'
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import ThemeToggle from './components/ThemeToggle';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function App() {
   return (
