fd40d830f1fa51cc426abee50c87a09495ada22a
santwan <santwanpathak1@gmail.com>
2025-04-11 12:17:26 +0530

    No changes in Code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import HeroSection from "../components/Home/HeroSection.jsx";
+import TweetSection from "../components/Home/TweetSection.jsx";
+import TweetLove from "../components/Home/TweetSection.jsx";
 import "./Home.css";
 
 const Home = () => {
   return (
     <> 
       <HeroSection/>
+      <TweetSection/>
+      
     </>
   )
 }
