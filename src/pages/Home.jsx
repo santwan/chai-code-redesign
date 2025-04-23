07b06bdeaa69ddc68c0c3f275c33d342aca27d70
santwan <santwanpathak1@gmail.com>
2025-04-23 13:07:31 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,26 @@
 import HeroSection from "../components/Home/HeroSection.jsx";
 import TweetSection from "../components/Home/TweetSection.jsx";
-import TweetLove from "../components/Home/TweetSection.jsx";
 import "./Home.css";
+import CohortsSection from "../components/Home/CohortSection.jsx";
+import StudentAchievement from "../components/Home/StudentAchievement.jsx";
+import TestimonialsSection from "../components/Home/TestimonialSection.jsx";
+import UdemySection from "../components/Home/UdemySection.jsx";
+import CohortBenefits from "../components/Home/BenefitSection.jsx";
+import WhyChaiCode from "../components/Home/WhyChaiCode.jsx";
 
 const Home = () => {
   return (
     <> 
       <HeroSection/>
       <TweetSection/>
+      <StudentAchievement/>
+      <CohortsSection/>
+      <TestimonialsSection/>
+      <UdemySection/>
+      <CohortBenefits/>
+      <WhyChaiCode/>
+
+      
       
     </>
   )
